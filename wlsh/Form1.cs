42526404ffc0a2323142f5edcbc646948338e670
soj <soj@163.com>
2019-09-16 11:19:39 +0800

    0916 1119

--- wlsh/Form1.cs
+++ wlsh/Form1.cs
@@ -12,6 +12,11 @@ namespace wlsh
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// 主线程 定时显示运行时间
+        /// </summary>
+        private Thread thread;
+        private delegate void changeText(string result);
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +27,9 @@ namespace wlsh
             Thread thread = new Thread(threadStart);
             thread.Name = "wodegaozhanghao";
             thread.Start();
+            ThreadStart threadStart2 = new ThreadStart(foo);
+            this.thread = new Thread(threadStart2);
+            this.thread.Start();
         }
         private void shzhong() {
             long ks = MyFuncUtil.GetTimestamp();
@@ -36,6 +44,40 @@ namespace wlsh
                         System.Diagnostics.Process.Start("shutdown.exe", "-r -f -t 15");
                     }
                 }
+            }
+        }
+        private void foo()
+        {
+
+            var ks = MyFuncUtil.GetTimestamp();
+            var ks_gxyunxing = MyFuncUtil.GetTimestamp();
+            var ks_cqyunxing = MyFuncUtil.GetTimestamp();
+            var i = 1;
+            string zidong = "";
+            while (true)
+            {
+                Thread.Sleep(1000);
+                var js = MyFuncUtil.GetTimestamp();
+                i++;
+
+                //MyFuncUtil.SecondToHour(+i + (js - ks) / 1000+" "
+                CalcFinished("程序已运行:" + MyFuncUtil.SecondToHour(js - ks) + zidong);
+                this.label1.ForeColor = Color.Red;
+                if ((js - ks) > 1000 * 10 && (js - ks) <= 1000 * 20)
+                {
+                    WriteLog.WriteLogFile("程序搞自动倒计时 " + (20 - (js - ks) / 1000) + "秒");
+                }
+            }
+        }
+        private void CalcFinished(string result)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new changeText(CalcFinished), result);
+            }
+            else
+            {
+                this.label1.Text = result.ToString();
             }
         }
     }
